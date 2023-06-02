a27d0edeb32f57a34c3c4857acf01d2809f98c56
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-06-02 01:56:09 -0700

    new update

--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -18,33 +18,16 @@ namespace RabbitMQ.Subscriber
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
 
-
-
-            //Her bir Subscribere nece dene mesaj gedeciyinin bolgusun bu method ile ediriy.
-            //Qebul edilen ilk parametir gonderilen mesajin olcusunu bildirir "0" yazasaq bu o demekdir ki
-            //istenilen olcude melumat qebul edirem.
-            //Ikinci parametir Subscriber nece eded mesaj qebul edecek onu bildirir.
-            //bool global => Ucuncu parmetir onu bildirirki eger biz Burada True Versek Deyekki her Subscribere 6 eded mesaj gedecey
-            //ve global parametrinide True-ya set etmisem bu zaman Bizim nece Eded Subscriberim var ona uygun olaraq bolub eyni anda
-            //gonderecey mesel ucun 2 eded Subscireber varsa eyni anda 3 mesaj birine 3 mesaj digerine gonderecek.
-            //Yox eger Global parametresini False-sa set etsek Tek bir seferde 1-ci Subscribere 6 eded mesaj gonderecek
-            //Sora yene mesajlar olsa tek seferde 2-ci Subscribere gonderecek ve bu bele Subscriberlerin sayina gore davam edecek.
+        
             channel.BasicQos(0, 1, false);
 
-            //Subscriber(consumer) yaradirig
             var consumer = new EventingBasicConsumer(channel);
-
             var queueName = "direct-queue-Error";
 
-            //Consumer ile Novbe arasinda ki baglantini qurmag ucun asagidaki koddan istifade olunur.
-            //bool AutoAck => adli ikinci paremtire Eger ki True versek  RabbitMQ Subscribere bir mesaj gonderiyi zaman
-            //bu mesaj dogruda olsa yanlisda olsa Novbeden silinecek. Eger ki bunu false versey biz demis oluruq ki 
-            //sen bunu novbeden silme eger ki mesaj dogru olsa men seni xeberdar edecem Novbeden silmeyin ucun.
             channel.BasicConsume(queueName, false, consumer);
 
             Console.WriteLine("Loglari dinliyorun...");
 
-            //Event uzerinnen Qulax asma prosesini yazmaq
             consumer.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
@@ -54,11 +37,6 @@ namespace RabbitMQ.Subscriber
 
                 File.AppendAllText("log-critical.txt", message + "\n");
 
-                //Bu method eger ki Novbeden geden mesaji hell ede bilibse bu zaman RabbitMQ-nu xebardar edirki
-                //artiq sen elaqeli mesaji sile bilersen ve silir.
-                //Sildikden sora burada ikinci parametre olaraq Multiple deyeri var. Eger buna true desek
-                //o an hell edilmis ama RabbitMQ-ya catmamis basqa mesajdar varsa onun melumatlarini
-                //RabbitMQ-ya xebardar eder.
                 channel.BasicAck(e.DeliveryTag, false);
             };
             Console.ReadLine();
