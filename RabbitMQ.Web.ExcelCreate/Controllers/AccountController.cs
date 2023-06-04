31add2d39b9f4fcd94e6d62946fa5873a9182651
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-06-04 04:26:29 -0700

    Add AccountController and ProductController

---
+++ RabbitMQ.Web.ExcelCreate/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Web.ExcelCreate.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var hasUser = await this.userManager.FindByEmailAsync(email);
+
+            if (hasUser == null)
+                return View();
+
+            var signInResult = await this.signInManager.PasswordSignInAsync(hasUser, password, true, false);
+            
+            if(!signInResult.Succeeded)
+                return View(signInResult);
+
+            return RedirectToAction(nameof(HomeController.Index),"Home");
+        }
+    }
+}
