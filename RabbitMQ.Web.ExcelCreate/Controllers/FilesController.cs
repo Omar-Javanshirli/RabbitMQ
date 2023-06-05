76795d63e2c37eb30f1fe766dd5c9f9545efdef9
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-06-05 01:18:55 -0700

    Add FileController in RabbitMQExcelCreate project

---
+++ RabbitMQ.Web.ExcelCreate/Controllers/FilesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQ.Web.ExcelCreate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesControlles : ControllerBase
+    {
+    }
+}
